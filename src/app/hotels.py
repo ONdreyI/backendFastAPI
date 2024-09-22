a0de1562987f2f0f48ec95ae495f46feef08f8cf
psk-a <ai3501669@gmail.com>
2024-09-22 13:18:12 +0300

    add fiter by id and title

--- src/app/hotels.py
+++ src/app/hotels.py
@@ -31,8 +31,18 @@ async def get_hotels(
     id: int | None = Query(None, description="Айдишник"),
     title: str | None = Query(None, description="Название отеля"),
 ):
+    per_page = pagination.per_page or 5
     async with async_session_maker() as session:
         query = select(HotelsOrm)
+        if id:
+            query = query.filter_by(id=id)
+        if title:
+            query = query.filter_by(title=title)
+        query = (
+            query
+            .limit(per_page)
+            .offset(per_page * (pagination.page-1))
+        )
         result = await session.execute(query)
         print(type(result), result)
         hotels = result.scalars().all()
