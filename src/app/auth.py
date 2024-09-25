d827c6aecc6d887f5a51357a43f4e9832cf56637
psk-a <ai3501669@gmail.com>
2024-09-25 10:01:24 +0300

    Создан jwt хэш паролей

--- src/app/auth.py
+++ src/app/auth.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+from passlib.context import CryptContext
+from jwt.exceptions import InvalidTokenError
 
 from backendCourse.repositories.users import UsersRepository
 from backendCourse.src.database import async_session_maker
@@ -6,12 +8,14 @@ from backendCourse.src.schemas.users import UserRequestAdd, UserAdd
 
 router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 
 @router.post("/register")
 async def register_user(
     data: UserRequestAdd,
 ):
-    hashed_password = "nlo3497pahfp437dsp"
+    hashed_password = pwd_context.hash(data.password)
     new_user_deta = UserAdd(email=data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
         await UsersRepository(session).add(new_user_deta)
