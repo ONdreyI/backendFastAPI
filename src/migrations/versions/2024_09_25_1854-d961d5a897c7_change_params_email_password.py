9d3b115c4279b4f2e7df9a178d110e748396138f
psk-a <ai3501669@gmail.com>
2024-09-25 19:14:21 +0300

    add unique params

---
+++ src/migrations/versions/2024_09_25_1854-d961d5a897c7_change_params_email_password.py
@@ -0,0 +1,28 @@
+"""change params email&password
+
+Revision ID: d961d5a897c7
+Revises: ec2648c45289
+Create Date: 2024-09-25 18:54:14.561150
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = "d961d5a897c7"
+down_revision: Union[str, None] = "ec2648c45289"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_unique_constraint(None, "users", ["email"])
+    op.create_unique_constraint(None, "users", ["hashed_password"])
+
+
+def downgrade() -> None:
+    op.drop_constraint(None, "users", type_="unique")
+    op.drop_constraint(None, "users", type_="unique")
