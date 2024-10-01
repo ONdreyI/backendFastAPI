dad7c98e23caaada6f7631afe5e9b80fd26e8005
psk-a <ai3501669@gmail.com>
2024-10-01 20:36:15 +0300

    Изменение rooms schema

--- src/schemas/rooms.py
+++ src/schemas/rooms.py
@@ -1,11 +1,11 @@
-from pydantic import Field, BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict
 
 
 class RoomAdd(BaseModel):
 
     hotel_id: int
     title: str
-    description: str
+    description: str | None = None
     price: int
     quantity: int
 
@@ -17,7 +17,8 @@ class Room(RoomAdd):
 
 
 class RoomPATCH(BaseModel):
-    title: str | None = Field(default=None)
-    description: str | None = Field(default=None)
-    price: int | None = Field(default=None)
-    quantity: int | None = Field(default=None)
+    hotel_id: int | None = None
+    title: str | None = None
+    description: str | None = None
+    price: int | None = None
+    quantity: int | None = None
