4d9aa4ff617869c985979df116d081e296da87c6
psk-a <ai3501669@gmail.com>
2024-10-02 14:22:24 +0300

    Добавление контекстного менеджера в ручку "/login"

--- src/app/auth.py
+++ src/app/auth.py
@@ -38,27 +38,25 @@ async def register_user(
 
 @router.post("/login")
 async def login_user(
+    db: DBDep,
     data: UserRequestAdd,
     response: Response,
 ):
-    async with async_session_maker() as session:
-        user = await UsersRepository(session).get_user_with_hashed_password(
-            email=data.email
+    user = await db.users.get_user_with_hashed_password(email=data.email)
+    if not user:
+        raise HTTPException(
+            status_code=401, detail="Пользователь с таким email не существует"
         )
-        if not user:
-            raise HTTPException(
-                status_code=401, detail="Пользователь с таким email не существует"
-            )
-        if not AuthService().verify_password(
-            data.password, hashed_password=user.hashed_password
-        ):
-            raise HTTPException(status_code=401, detail="Неверный пароль")
-        access_token = AuthService().create_access_token({"user_id": user.id})
-        response.set_cookie(
-            key="access_token",
-            value=access_token,
-        )
-        return {"access_token": access_token}
+    if not AuthService().verify_password(
+        data.password, hashed_password=user.hashed_password
+    ):
+        raise HTTPException(status_code=401, detail="Неверный пароль")
+    access_token = AuthService().create_access_token({"user_id": user.id})
+    response.set_cookie(
+        key="access_token",
+        value=access_token,
+    )
+    return {"access_token": access_token}
 
 
 @router.get("/me")
