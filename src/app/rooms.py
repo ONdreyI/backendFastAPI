4b2b6873d9448ec4c96ac2ad93a607a7baf2da95
psk-a <ai3501669@gmail.com>
2024-10-02 14:54:16 +0300

    Добавлен контекстный менеджер для метода PATCH.

--- src/app/rooms.py
+++ src/app/rooms.py
@@ -1,6 +1,12 @@
 from fastapi import APIRouter, Query, Body
-from backendCourse.src.app.dependencies import PaginationDep
-from backendCourse.src.schemas.rooms import Room, RoomAdd, RoomPatch, RoomPatchRequest
+from backendCourse.src.app.dependencies import PaginationDep, DBDep
+from backendCourse.src.schemas.rooms import (
+    Room,
+    RoomAdd,
+    RoomAddRequest,
+    RoomPatch,
+    RoomPatchRequest,
+)
 
 from backendCourse.src.database import async_session_maker
 
@@ -14,21 +20,27 @@ router = APIRouter(
 
 
 @router.get("/{hotel_id}/rooms", name="Получение списка номеров при отеле")
-async def get_rooms(hotel_id: int):
-    async with async_session_maker() as session:
-        return await RoomsRepository(session).get_filtered(hotel_id=hotel_id)
+async def get_rooms(
+    db: DBDep,
+    hotel_id: int,
+):
+    return await db.rooms.get_filtered(hotel_id=hotel_id)
 
 
 @router.get("/{hotel_id}/rooms/{room_id}", name="Получение одного отеля")
-async def get_hotel(hotel_id: int, room_id: int):
-    async with async_session_maker() as session:
-        return await RoomsRepository(session).get_one_or_none(id=room_id)
+async def get_hotel(
+    db: DBDep,
+    hotel_id: int,
+    room_id: int,
+):
+    return await db.rooms.get_one_or_none(id=room_id)
 
 
 @router.post("/{hotel_id}/rooms", name="Add room data")
 async def create_room(
+    db: DBDep,
     hotel_id: int,
-    room_data: RoomAdd = Body(
+    room_data: RoomAddRequest = Body(
         openapi_examples={
             "1": {
                 "summary": "Sochi Hotel",
@@ -54,25 +66,25 @@ async def create_room(
     ),
 ):
     _room_data = RoomAdd(hotel_id=hotel_id, **room_data.model_dump())
-    async with async_session_maker() as session:
-        room = await RoomsRepository(session).add(_room_data)
-        await session.commit()
-
+    room = await db.rooms.add(_room_data)
+    await db.commit()
     return {"status": "Ok", "data": room}
 
 
-#
-#
 @router.put("/{hotel_id}/rooms/{room_id}")
-async def put_hotel(hotel_id: int, room_id: int, room_data: RoomAdd):
+async def put_hotel(
+    db: DBDep,
+    hotel_id: int,
+    room_id: int,
+    room_data: RoomAddRequest,
+):
     _room_data = RoomAdd(hotel_id=hotel_id, **room_data.model_dump())
-    async with async_session_maker() as session:
-        await RoomsRepository(session).update(
-            id=room_id,
-            data=_room_data,
-        )
-        await session.commit()
-        return {"status": "updated"}
+    await db.rooms.update(
+        _room_data,
+        id=room_id,
+    )
+    await db.commit()
+    return {"status": "updated"}
 
 
 @router.patch(
@@ -81,6 +93,7 @@ async def put_hotel(hotel_id: int, room_id: int, room_data: RoomAdd):
     description="<h1>Можно изменить только часть полей номера</h1>",
 )
 async def patch_hotel(
+    db: DBDep,
     hotel_id: int,
     room_id: int,
     room_data: RoomPatchRequest,
@@ -89,15 +102,14 @@ async def patch_hotel(
         hotel_id=hotel_id,
         **room_data.model_dump(exclude_unset=True),
     )
-    async with async_session_maker() as session:
-        await RoomsRepository(session).update(
-            _room_data,
-            id=room_id,
-            exclude_unset=True,
-            hotel_id=hotel_id,
-        )
-        await session.commit()
-        return {"status": "updated"}
+    await db.rooms.update(
+        _room_data,
+        id=room_id,
+        exclude_unset=True,
+        hotel_id=hotel_id,
+    )
+    await db.commit()
+    return {"status": "updated"}
 
 
 @router.delete("/{hotel_id}/rooms/{room_id}")
