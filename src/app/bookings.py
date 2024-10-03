be604ce25a96ce26a1fcf7c50d2ba850613b392a
psk-a <ai3501669@gmail.com>
2024-10-03 11:00:31 +0300

    Добавление ручек GET_me, GET_all

--- src/app/bookings.py
+++ src/app/bookings.py
@@ -10,45 +10,42 @@ from backendCourse.src.schemas.bookings import (
 
 
 router = APIRouter(
-    prefix="/hotels",
+    prefix="/bookings",
     tags=["Бронирование"],
 )
 
 
-# @router.get("/{hotel_id}/rooms", name="Получение списка номеров при отеле")
-# async def get_rooms(
-#     db: DBDep,
-#     hotel_id: int,
-# ):
-#     return await db.rooms.get_filtered(hotel_id=hotel_id)
+@router.get("/GET/bookings/me", name="Получение только своих бронирований")
+async def get_rooms(
+    db: DBDep,
+    user_id: int,
+):
+    return await db.bookings.get_filtered(user_id=user_id)
 
 
-# @router.get("/{hotel_id}/rooms/{room_id}", name="Получение одного отеля")
-# async def get_hotel(
-#     db: DBDep,
-#     hotel_id: int,
-#     room_id: int,
-# ):
-#     return await db.rooms.get_one_or_none(id=room_id)
+@router.get("/GET/bookings", name="Получение всех бронирований")
+async def get_hotel(
+    db: DBDep,
+):
+    return await db.bookings.get_all()
 
 
 @router.post(
-    "/hotels/{hotel_id}/rooms/{room_id}/bookings",
+    "",
     name="Add booking data",
 )
 async def create_room(
     db: DBDep,
-    room_id: int,
     user_id: int,
     booking_data: BookingAddRequest = Body(
         openapi_examples={
             "1": {
                 "summary": "Sochi Hotel",
                 "value": {
+                    "room_id": 4,
                     "date_from": "2024-10-05",
                     "date_to": "2024-10-25",
                     "description": "A luxurious room in Sochi Hotel",
-                    "price": 100,
                 },
             },
             "2": {
@@ -65,12 +62,19 @@ async def create_room(
         }
     ),
 ):
+    # Получить схему номера
+    # Создать схему данных BookingAdd
+    # добавить бронирование конкретному пользователю.
+    room = await db.rooms.get_one_or_none(id=booking_data.room_id)
+    room_price: int = room.price
     _booking_data = BookingAdd(
-        room_id=room_id, user_id=user_id, **booking_data.model_dump()
+        user_id=user_id,
+        price=room_price,
+        **booking_data.model_dump(),
     )
-    room = await db.bookings.add(_booking_data)
+    booking = await db.bookings.add(_booking_data)
     await db.commit()
-    return {"status": "Ok", "data": room}
+    return {"status": "Ok", "data": booking}
 
 
 # @router.put("/{hotel_id}/rooms/{room_id}")
