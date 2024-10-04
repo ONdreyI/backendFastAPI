a8f365ff785d2735543691eca184e150fea0213b
psk-a <ai3501669@gmail.com>
2024-10-04 14:44:58 +0300

    Вынесение кода в общий файл для совместного использования внутри src.repositories.

--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -7,6 +7,7 @@ from backendCourse.src.database import engine
 from backendCourse.src.models.bookings import BookingsOrm
 from backendCourse.src.repositories.base import BaseRepository
 from backendCourse.src.models.rooms import RoomsOrm
+from backendCourse.src.repositories.utils import rooms_ids_for_booking
 from backendCourse.src.schemas.rooms import Room
 
 
@@ -34,40 +35,10 @@ class RoomsRepository(BaseRepository):
         SELECT * from rooms_left_table
         WHERE rooms_left > 0 AND room_id in (SELECT id FROM rooms WHERE hotel_id = 16);
         """
-        rooms_count = (
-            select(BookingsOrm.room_id, func.count("*").label("rooms_booked"))
-            .select_from(BookingsOrm)
-            .filter(
-                BookingsOrm.date_to >= date_to,
-                BookingsOrm.date_from <= date_from,
-            )
-            .group_by(BookingsOrm.room_id)
-            .cte(name="rooms_count")
-        )
-        rooms_left_table = (
-            select(
-                RoomsOrm.id.label("room_id"),
-                (
-                    RoomsOrm.quantity - func.coalesce(rooms_count.c.rooms_booked, 0)
-                ).label("rooms_left"),
-            )
-            .select_from(RoomsOrm)
-            .outerjoin(rooms_count, RoomsOrm.id == rooms_count.c.room_id)
-            .cte(name="rooms_left_table")
-        )
-        rooms_ids_for_hotel = (
-            select(RoomsOrm.id)
-            .select_from(RoomsOrm)
-            .filter_by(hotel_id=hotel_id)
-            .subquery(name="rooms_ids_for_hotel")
-        )
-        rooms_ids_to_get = (
-            select(rooms_left_table.c.room_id)
-            .select_from(rooms_left_table)
-            .filter(
-                rooms_left_table.c.rooms_left > 0,
-                rooms_left_table.c.room_id.in_(rooms_ids_for_hotel),
-            )
+        rooms_ids_to_get = rooms_ids_for_booking(
+            date_from,
+            date_to,
+            hotel_id,
         )
 
         return await self.get_filtered(RoomsOrm.id.in_(rooms_ids_to_get))
