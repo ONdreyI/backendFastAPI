0bef704a3d9ecd09b3ccffc8640304ae3d850fd6
psk-a <ai3501669@gmail.com>
2024-10-06 14:31:25 +0300

    Замена autoincrement на next.val для перезаписи удаленного номера primery key

--- src/models/facilities.py
+++ src/models/facilities.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, ForeignKey
+from sqlalchemy import String, ForeignKey, Sequence
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.database import Base
@@ -12,6 +12,9 @@ class FacilitiesOrm(Base):
 
 class RoomsFacilitiesOrm(Base):
     __tablename__ = "rooms_facilities"
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+        server_default=Sequence("rooms_facilities_id_seq").next_value(),
+    )
     room_id: Mapped[int] = mapped_column(ForeignKey("rooms.id"))
     facility_id: Mapped[int] = mapped_column(ForeignKey("facilities.id"))
