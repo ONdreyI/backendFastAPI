a52818d5a738c10719556a16024af7e83044ffdc
psk-a <ai3501669@gmail.com>
2024-10-06 10:00:03 +0300

    Изменены названия таблиц

--- src/models/facilities.py
+++ src/models/facilities.py
@@ -4,13 +4,13 @@ from sqlalchemy.orm import Mapped, mapped_column
 from src.database import Base
 
 
-class Facilities(Base):
+class FacilitiesOrm(Base):
     __tablename__ = "facilities"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str] = mapped_column(String(100))
 
 
-class RoomsFacilities(Base):
+class RoomsFacilitiesOrm(Base):
     __tablename__ = "rooms_facilities"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     room_id: Mapped[int] = mapped_column(ForeignKey("rooms.id"))
