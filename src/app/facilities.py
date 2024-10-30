648072bb2b4cdda539d6fecde0bcce65bc8c3433
psk-a <ai3501669@gmail.com>
2024-10-30 15:50:17 +0300

    Redis для ручки get facilities

--- src/app/facilities.py
+++ src/app/facilities.py
@@ -22,7 +22,17 @@ router = APIRouter(
 
 @router.get("")
 async def get_facilities(db: DBDep):
-    return await db.facilities.get_all()
+    facilities_from_cache = await redis_manager.get("facilities")
+    if not facilities_from_cache:
+        print("ИДУ В БАЗУ ДАННЫХ")
+        facilities = await db.facilities.get_all()
+        facilities_schemas: list[dict] = [f.model_dump() for f in facilities]
+        facilities_json = json.dumps(facilities_schemas)
+        await redis_manager.set("facilities", facilities_json, 10)
+        return facilities
+    else:
+        facilities_dicts = json.loads(facilities_from_cache)
+        return facilities_dicts
 
 
 # @router.get("", name="Получение одного удобства")
