e05c85643a82ba55b2a5ce61607e639e077e843a
psk-a <ai3501669@gmail.com>
2024-11-09 10:06:39 +0300

    Изменение способа создания ID для RoomsFacilities ORM

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,16 @@
-from src.models.hotels import HotelsOrm
-from src.models.rooms import RoomsOrm
-from src.models.users import UsersOrm
-from src.models.bookings import BookingsOrm
-from src.models.facilities import FacilitiesOrm
+# backend/src/models/__init__.py
+from .hotels import HotelsOrm
+from .rooms import RoomsOrm
+from .users import UsersOrm
+from .bookings import BookingsOrm
+from .facilities import FacilitiesOrm, RoomsFacilitiesOrm
+
+__all__ = [
+    "HotelsOrm",
+    "RoomsOrm",
+    "UsersOrm",
+    "BookingsOrm",
+    "FacilitiesOrm",
+    "RoomsFacilitiesOrm"
+]
+
