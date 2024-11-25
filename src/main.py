b6c106d6c5e3dd27fdb96522ed0f018677f50e0f
psk-a <ai3501669@gmail.com>
2024-11-25 07:01:14 +0300

    Подстройка редиса под статический типизатор

--- src/main.py
+++ src/main.py
@@ -25,7 +25,7 @@ from src.app.images import router as router_images
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await redis_manager.connect()
-    FastAPICache.init(RedisBackend(redis_manager.redis), prefix="fastapi-cache")
+    FastAPICache.init(RedisBackend(redis_manager._redis), prefix="fastapi-cache")
     yield
     await redis_manager.close()
 
