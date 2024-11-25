c560a5f0c5195f0f5abe6a3f8473f9cf2e361b3d
psk-a <ai3501669@gmail.com>
2024-11-25 14:41:13 +0300

    Добавление тестов для ручек delete, patch в tests/hotels

--- tests/integration_tests/hotels/test_api.py
+++ tests/integration_tests/hotels/test_api.py
@@ -8,3 +8,21 @@ async def test_get_hotels(ac):
     )
     print(f"{response.json()=}")
     assert response.status_code == 200
+
+
+async def test_patch_hotels(ac):
+    hotel_id = 2  # Replace with the actual hotel ID
+    response = await ac.patch(
+        f"/hotels/{hotel_id}",
+        json={"title": "New Hotel Title"},
+    )
+    print(f"{response.json()=}")
+    assert response.status_code == 200
+    # assert response.json().get("title")[0] == "New Hotel Title"
+
+
+async def test_delete(ac):
+    hotel_id = 1  # Replace with the actual hotel ID
+    response = await ac.delete(f"/hotels/{hotel_id}")
+    assert response.status_code == 200
+    assert response.json().get("status") == "deleted"
